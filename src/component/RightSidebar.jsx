b1cd2e6a30dfd4b9421e47a6d0d293b7bb225ccf
kashifmurtaza72 <70048358+kashifmurtaza72@users.noreply.github.com>
2025-03-06 07:59:57 +0500

    searching...

--- src/component/RightSidebar.jsx
+++ src/component/RightSidebar.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+"use client";
+//when we have interaction in our website, clicking, submission, if we use states, we need to add "use client"
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function RightSidebar() {
+  const [input, setInput] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!input.trim()) return;
+    router.push(`/search/${input}`);
+  };
   return (
-    <div>RightSidebar</div>
-  )
+    <>
+      <div className="sticky top-0 py-2 bg-white">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="search"
+            value={input} className="bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2"
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </form>
+      </div>
+    </>
+  );
 }
